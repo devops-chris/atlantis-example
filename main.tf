92f60f1f1f9eedb1055af315a3d71ecec6fc6f8b
Chris Lindsay <christopher.m.lindsay@gmail.com>
2020-06-27 00:02:03 -0400

    Update main.tf
    yuck

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "cms-dev-test-terraform-state"
+    region         = "us-east-1"
+    dynamodb_table = "tf_lock"
+  }
+}
+
 resource "null_resource" "example" {
 }
 
